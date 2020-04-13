e0e5798c25dd5b0ad65ec5db6b20a32d752e12b5
AlizeDeMatas <alize.dematas1@gmail.com>
2020-04-13 02:13:43 -0400

    Updates

---
+++ precache-manifest.ddbfa05bfeac03b6d707fb2d53bb3313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457bff1ad7b6c5c9453f0c3854976882",
+    "url": "/TheSocialNetwork/index.html"
+  },
+  {
+    "revision": "d0d71d02d4a808e9db7c",
+    "url": "/TheSocialNetwork/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ee0ac9f871834cad4a24",
+    "url": "/TheSocialNetwork/static/css/main.58b1b952.chunk.css"
+  },
+  {
+    "revision": "d0d71d02d4a808e9db7c",
+    "url": "/TheSocialNetwork/static/js/2.87bc2a91.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TheSocialNetwork/static/js/2.87bc2a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee0ac9f871834cad4a24",
+    "url": "/TheSocialNetwork/static/js/main.c2649af7.chunk.js"
+  },
+  {
+    "revision": "a0432451312bb254d37d",
+    "url": "/TheSocialNetwork/static/js/runtime-main.b03f5b45.js"
+  }
+]);
